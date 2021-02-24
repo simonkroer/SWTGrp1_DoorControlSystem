4a538bfbadac4fde13e48e00fc13c5f6cfffc2e9
Thomas Daugaard <thomasmdaugaard@gmail.com>
2021-02-24 10:07:23 +0100

    Added method RaiseAlarm() to Alarm class

--- DoorControlSystem/Alarm.cs
+++ DoorControlSystem/Alarm.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace DoorControlSystem
 {
     class Alarm
     {
+        public void RaiseAlarm()
+        {
+            Console.WriteLine("Alarm raised");
+        }
     }
 }
