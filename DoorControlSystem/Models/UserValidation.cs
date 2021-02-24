6d9cc75c0bfb21edae6f92c7d003731991ed9c9f
Egarder <61559853+Egarder@users.noreply.github.com>
2021-02-24 10:54:06 +0100

    added ti uservalid

--- DoorControlSystem/Models/UserValidation.cs
+++ DoorControlSystem/Models/UserValidation.cs
@@ -1,13 +1,34 @@
-﻿using DoorControlSystem.Interfaces;
+﻿using System;
+using DoorControlSystem.Interfaces;
+using System.Collections.Generic;
 
 namespace DoorControlSystem.Models
 {
     public class UserValidation : IUserValidation
     {
+        UserValidation()
+        {
+            id_.Add("simon");
+            id_.Add("camilla");
+            id_.Add("thomas");
+            id_.Add("emil");
+        }
         public bool ValidateEntryRequest(string id)
         {
-            return true;
+            foreach (string element in id_)
+            {
+               bool found= element.Equals(id);
+
+               if (found == true)
+               {
+                   return true;
+               }
+
+            }
+            return false;
         }
 
+        private List<string> id_;
     }
+
 }
