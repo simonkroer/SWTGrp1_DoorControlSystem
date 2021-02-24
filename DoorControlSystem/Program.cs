210ab946f3bbb1e30095bbfcc54fd10ef7510e03
simonkroer <simonkroer@gmail.com>
2021-02-24 09:16:01 +0100

    Add project files.

---
+++ DoorControlSystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DoorControlSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
